329093555e320aada247d00afc2ac09000caea78
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-02-23 18:48:54 +0530

    variables && datatypes

---
+++ Basics/01.variables.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // printing statement
+    // cout<<"Hello\n" ;
+
+    // variable declaration & assignment
+    // int took 4 bytes to store it converts into bits cause computer understand only machine language 32 bits
+
+    int a = 5;
+    cout<<a + 3;
+
+  // flaot took 4 bytes : 32 bits
+    float b = 3.4;
+    cout<<b<<endl;
+
+// char took 1 byte : 8 bits
+    char c = 'a';
+    cout<<c<<endl;
+
+// bool took 1 byte : 8 bits
+    bool d = 1, e = true, f = false; 
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+
+// double took 8bytes : 64 bits
+    double g = 1234567;
+    
+}
