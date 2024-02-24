9b1fa7a000a958a00caa9490c667b14572feb3a9
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-02-24 20:04:57 +0530

    ifelse

---
+++ Basics/02.ifElse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter first number to add ";
+    cin>>a;
+    cout<<"Enter second number to add ";
+    cin>>b;
+
+    cout<<"sum of number is: "<<a+b;
+
+}
